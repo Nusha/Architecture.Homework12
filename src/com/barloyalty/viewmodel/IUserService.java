d5128e5daf956595fd36809fd9bad3c99a995dfa
Ivan Balabay <i.balabai@gmail.com>
2023-09-11 18:22:23 +0300

    Additional methods added to interface

--- src/com/barloyalty/viewmodel/IUserService.java
+++ src/com/barloyalty/viewmodel/IUserService.java
@@ -3,6 +3,9 @@ package com.barloyalty.viewmodel;
 import com.barloyalty.model.User;
 
 public interface IUserService {
-    void register(String username, String password);
-    User login(String username, String password);
+    void register(String username, String password); // метод для регистрации пользователя
+    User login(String username, String password); // метод для авторизации пользователя
+    void logout(String username);  // метод для выхода из системы
+    void resetPassword(String username);  //  метод для сброса пароля
+    void updateUserDetails(String username, String details);  // метод для обновления информации о пользователе
 }

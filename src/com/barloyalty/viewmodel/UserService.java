62387da77f52e570daf1ce0269274ab8f2ccbf2a
Ivan Balabay <i.balabai@gmail.com>
2023-09-11 18:26:13 +0300

    Additional methods added to UserService

--- src/com/barloyalty/viewmodel/UserService.java
+++ src/com/barloyalty/viewmodel/UserService.java
@@ -8,11 +8,28 @@ public class UserService  implements IUserService{
     private List<User> users;
     @Override
     public void register(String username, String password) {
-
+// реализация
     }
 
     @Override
-    public User login(String username, String password) {
+    public User login(String username, String password)  {
+        // реализация
         return null;
     }
+
+    @Override
+    public void logout(String username) {
+        // реализация
+    }
+
+    @Override
+    public void resetPassword(String username) {
+        // реализация
+    }
+
+    @Override
+    public void updateUserDetails(String username, String details) {
+        //реализация
+    }
+
 }

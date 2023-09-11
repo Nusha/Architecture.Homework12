9dfeec9cd1f6bda6896bdf0a5ada9f6fc19c1995
Ivan Balabay <i.balabai@gmail.com>
2023-09-11 18:28:55 +0300

    Additional methods added to interface

--- src/com/barloyalty/viewmodel/IProductService.java
+++ src/com/barloyalty/viewmodel/IProductService.java
@@ -5,5 +5,9 @@ import com.barloyalty.model.Product;
 import java.util.List;
 
 public interface IProductService {
-    List<Product> search(String query);
+    List<Product> search(String query); // метод для поиска продукта в корзину
+    void addProductToCart(String productId);  // метод для добавления продукта в корзину
+    void removeProductFromCart(String productId);  //  метод для удаления продукта из корзины
+    void checkout();  //  метод для оформления заказа
+    void applyDiscountCode(String code);  //  метод для применения скидочного кода
 }
